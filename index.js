0e8ac1da6757f6f5eacc6822724f018f0c73df39
anishatamhane <anisha.tamhane@mitwpu.edu.in>
2026-03-31 15:58:19 +0530

    Added ToDo REST API (assign6)

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 
 let tasks = [];
 
-// GET all tasks
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
-// POST new task
 app.post('/tasks', (req, res) => {
     const task = {
         id: tasks.length + 1,
@@ -21,25 +20,6 @@ app.post('/tasks', (req, res) => {
     res.json(task);
 });
 
-// PUT update task
-app.put('/tasks/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === id);
-
-    if (task) {
-        task.completed = req.body.completed;
-        res.json(task);
-    } else {
-        res.status(404).send("Task not found");
-    }
-});
-
-// DELETE task
-app.delete('/tasks/:id', (req, res) => {
-    tasks = tasks.filter(t => t.id !== parseInt(req.params.id));
-    res.send("Task deleted");
-});
-
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on http://localhost:3000");
 });
